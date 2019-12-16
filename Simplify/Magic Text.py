f177a996a29c495161f702e8ac8e81428ee2f113
KillerRhys <undead_atomsk@live.com>
2019-12-16 18:56:27 -0500

    Easy text editor used to make words.json over 100k words in 15 mins!

---
+++ Simplify/Magic Text.py
@@ -0,0 +1,25 @@
+import sys, time, os
+
+def cuts():
+    with open(r"/Users/Undea/Documents/words.txt") as f:
+        string = f.read()
+        print('Please select char number to cut!:')
+        n=int(input())
+        string2=[string[i:i+n] for i in range(0, len(string), n)]
+        with open('/Users/Undea/Documents/words2.txt', 'w') as f:
+            for item in string2:
+                f.write("%s\n" % item)
+    print("step 1 complete")
+
+
+
+def xfer():
+    with open(r"/Users/Undea/Documents/words2.txt") as f:
+        content = f.read().upper().splitlines()
+        string1 = '{"word": "'
+        string2 = '"},'
+        content2 = [string1 + x + string2 for x in content]
+        with open('/Users/Undea/Documents/words3', 'w') as f:
+            for item in content2:
+                f.write("%s\n" % item)
+    print('Finshed Tasks')
